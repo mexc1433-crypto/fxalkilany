d8505ac90e12465e00cbfc44b20bc083e285f7de
Hassanjj <mexc1433@gmail.com>
2026-07-19 03:38:14 +0300

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+        body: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
